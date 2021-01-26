39371b367799eef4ccf96164aac725052b8f81a0
Vasista Vovveti <vasistavovveti@gmail.com>
2021-01-25 23:09:11 -0600

    add maven scanner (#3)
    * add maven scanner
    
    * dont require checkout
    
    * fix typo
    
    * bark
    
    * fix logs
    
    * add more logging
    
    * dont create issue if no updates found
    
    * add dist script

---
+++ check-for-maven-update/index.js
@@ -0,0 +1,147 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const toml = require('toml');
+const fetch = require('node-fetch');
+const parseString = require('xml2js').parseString;
+const compareVersions = require('compare-versions');
+
+async function run() {
+    const githubToken = core.getInput('token');
+    const context = github.context;
+    const octokit = github.getOctokit(githubToken);
+
+    try {
+
+        let rpybuild;
+        await octokit.repos.getContent({
+            owner: context.repo.owner, // should be 'robotpy'
+            repo: context.repo.repo,
+            ref: context.ref,
+            path: 'pyproject.toml'
+        }).then(result => {
+            // content will be base64 encoded
+            const tomlString = Buffer.from(result.data.content, 'base64').toString();
+            const data = toml.parse(tomlString);
+            rpybuild = data["tool"]["robotpy-build"];
+        })
+        
+        let mavenLibDownloads = [];
+        
+        if (rpybuild["wrappers"]){
+            for (const [key, value] of Object.entries(rpybuild["wrappers"])) {
+                let mvl = value["maven_lib_download"];
+                if (mvl) {
+                    mavenLibDownloads.push(mvl);
+                }
+            }
+        }
+
+        if (rpybuild["static_libs"]){
+            for (const [key, value] of Object.entries(rpybuild["static_libs"])) {
+                let mvl = value["maven_lib_download"];
+                if (mvl) {
+                    mavenLibDownloads.push(mvl);
+                }
+            }
+        }
+        
+        core.info("maven_lib_downloads found:");
+        core.info(JSON.stringify(mavenLibDownloads));
+
+        var outOfDates = [];
+
+        for (const mvl of mavenLibDownloads) {
+            let currentVersion = mvl["version"];
+            let metadataUrl = `${mvl["repo_url"]}/${mvl["group_id"].replace(/\./g, "/")}/${mvl["artifact_id"]}/maven-metadata.xml`;
+            
+            await fetch(metadataUrl)
+            .then((resp) => resp.text())
+            .then(function(xml) {
+                
+                parseString(xml, (err, result) => {
+
+                    let releaseVersion = result["metadata"]["versioning"][0]["release"][0];
+                    if( compareVersions.compare(releaseVersion, currentVersion, '>')) {
+                        outOfDates.push({"artifact": mvl["artifact_id"], "currentVersion": currentVersion, "newVersion": releaseVersion});
+                    }
+
+                });
+            
+            })
+            .catch(function(error) {
+                core.error("Maven parsing failed!");
+                core.setFailed(error.message);
+            });
+
+        }
+
+        if (outOfDates.length == 0) {
+            core.info("no updates found.");
+            return;
+        }
+
+        outOfDates.sort((a, b) => a["artifact"].localeCompare(b["artifact"]));
+
+        core.info("updates found:");
+        core.info(JSON.stringify(outOfDates));
+
+        const issueTitle = "[nag] Update Maven Dependencies";
+        const issueBody = outOfDates.reduce( (out, ood) =>
+            out + `- ${ood["artifact"]}: ${ood["currentVersion"]} --> ${ood["newVersion"]}\n`
+        , "Maven library updates available:\n\n")
+
+        let existingIssue;
+        // refer https://github.com/JasonEtco/create-an-issue/blob/main/src/action.ts
+        try {
+            const existingIssues = await octokit.search.issuesAndPullRequests({
+                q: `is:open is:issue repo:${process.env.GITHUB_REPOSITORY} in:title ${issueTitle}`
+            });
+            existingIssue = existingIssues.data.items.find(issue => issue.title === issueTitle);
+        } catch (error) {
+            core.error("Issue fetching failed!");
+            core.setFailed(error.message);
+        }
+        
+        if (existingIssue) {
+            core.info("Existing issue found.");
+            if (existingIssue.body !== issueBody){
+                // update existing issue instead creating a new one
+                core.info("Updating existing issue.");
+                try {
+                    const issue = await octokit.issues.update({
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        issue_number: existingIssue.number,
+                        body: issueBody
+                    });
+                } catch (error) {
+                    core.error("Issue update failed!");
+                    core.setFailed(error.message);
+                }
+            } else {
+                core.info("No change. Existing issue is up-to-date.");
+            }
+        } else {
+            // create a new issue
+            core.info("Creating new issue.");
+            try {
+                const issue = await octokit.issues.create({
+                    owner: context.repo.owner,
+                    repo: context.repo.repo,
+                    title: issueTitle,
+                    body: issueBody
+                });  
+            } catch (error) {
+                core.error("Issue creation failed!");
+                core.setFailed(error.message);
+            }
+        }
+
+
+    } catch (error) {
+        core.error("Script failed!");
+        core.setFailed(error.message);
+    }
+}
+
+run()
