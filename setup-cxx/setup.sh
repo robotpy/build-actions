d7419aa9380253949cfda20db5beeabe731424e1
David Vo <auscompgeek@users.noreply.github.com>
2021-12-30 15:13:46 +1100

    Add setup-cxx action to install gcc-8 on bionic

---
+++ setup-cxx/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Safeguard in case this accidentally runs on Windows or macOS.
+if [ "$RUNNER_OS" != "Linux" ]; then
+	echo "On $RUNNER_OS, doing nothing."
+	exit
+fi
+
+sudo apt-get install --no-install-recommends g++-8 gcc-8
+
+DIR="$RUNNER_TEMP/gcc-override"
+mkdir "$DIR"
+ln -s /usr/bin/gcc-8 "$DIR/gcc"
+ln -s /usr/bin/g++-8 "$DIR/g++"
+echo "$DIR" >>"$GITHUB_PATH"
