9d4ad2fcb96dfdd7da4a062ea2326857c9b0e603
Dustin Spicuzza <dustin@virtualroadside.com>
2025-05-21 00:23:57 -0400

    Add install-semiwrap action

---
+++ install-semiwrap/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs').promises;
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+const toml = require('toml');
+
+async function run() {
+
+    // read toml file
+    try {
+        const pythonPath = core.getInput("python");
+        const tomlString = await fs.readFile("pyproject.toml");
+        const data = toml.parse(tomlString);
+        
+        // find semiwrap in dependencies
+        var found = false;
+        var dep = null;
+        var requires = data["build-system"]["requires"];
+
+        for (var i = 0; i < requires.length; i++) {
+            dep = requires[i];
+            if (dep.startsWith("semiwrap")) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            core.setFailed("could not find semiwrap in build dependencies");
+            return;
+        }
+
+        core.info("[install-semiwrap] semiwrap dependency found: " + dep);
+
+        // Run the installation
+        await exec.exec(pythonPath, ["-m", "pip", "--disable-pip-version-check", "install", dep]);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+run();
