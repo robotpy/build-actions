c9f6fc0551d13bdf530f5087f53d27f8faf5f059
Dustin Spicuzza <dustin@virtualroadside.com>
2026-01-09 01:27:33 -0500

    Update semiwrap install action

--- install-semiwrap/index.js
+++ install-semiwrap/index.js
@@ -8,6 +8,7 @@ async function run() {
     // read toml file
     try {
         const pythonPath = core.getInput("python");
+        const crossExpose = core.getInput("cross-expose");
         const tomlString = await fs.readFile("pyproject.toml");
         const data = toml.parse(tomlString);
         
@@ -33,6 +34,9 @@ async function run() {
 
         // Run the installation
         await exec.exec(pythonPath, ["-m", "pip", "--disable-pip-version-check", "install", dep]);
+        if (crossExpose != "") {
+          await exec.exec(crossExpose, ["semiwrap"]);
+        }
     } catch (error) {
         core.setFailed(error.message);
     }
